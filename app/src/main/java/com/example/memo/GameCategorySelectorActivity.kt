cd4c0c2123f331a4dd0d3b6879e3634a67265eb6
Oliwier <97135602+aeakii@users.noreply.github.com>
2023-02-03 19:22:28 +0100

    add gui

---
+++ app/src/main/java/com/example/memo/GameCategorySelectorActivity.kt
@@ -0,0 +1,11 @@
+package com.example.memo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameCategorySelectorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_category_selector)
+    }
+}
