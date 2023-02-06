9cd08b73334e8ae4d5558e0a0054b9a45d6ace07
Oliwier <97135602+aeakii@users.noreply.github.com>
2023-02-06 14:13:22 +0100

    switch between activities

--- app/src/main/java/com/example/memo/GameCategorySelectorActivity.kt
+++ app/src/main/java/com/example/memo/GameCategorySelectorActivity.kt
@@ -1,11 +1,17 @@
 package com.example.memo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class GameCategorySelectorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_category_selector)
+    }
+    fun startGame(view:View){
+        val game = Intent(this, GameActivity::class.java)
+        startActivity(game)
     }
 }
--- app/src/main/java/com/example/memo/MainActivity.kt
+++ app/src/main/java/com/example/memo/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.memo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun playSinglePlayer(view: View){
+        val gameCatSelector = Intent(this, GameCategorySelectorActivity::class.java)
+        startActivity(gameCatSelector)
     }
 }
